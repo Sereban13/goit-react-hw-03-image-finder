588a2ecc62b04e4b83a19a7e4b12d32a7e026710
Sehii Perevertniuk <s.perevertnyuk@gmail.com>
2023-08-27 20:20:11 +0200

    and one more

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,17 +1,17 @@
-const ImageGalleryItem = images => {
-  return (
-    <div>
-      <li key={id}>
-        {images.map(({ id, webformatURL }) => {
-          return (
-            <>
-              <img src={webformatURL} alt="" />;
-            </>
-          );
-        })}
-      </li>
-    </div>
-  );
-};
+// const ImageGalleryItem = images => {
+//   return (
+//     <div>
+//       <li key={id}>
+//         {images.map(({ id, webformatURL }) => {
+//           return (
+//             <>
+//               <img src={webformatURL} alt="" />;
+//             </>
+//           );
+//         })}
+//       </li>
+//     </div>
+//   );
+// };
 
 // export default ImageGalleryItem;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,52 +1,13 @@
-// import { render } from "@testing-library/react";
-// import { Component } from 'react';
-
-// export class Searchbar extends Component {
-//   state = {
-//     searchQuery: '',
-//   };
-//   // handleChange = ({ target: { value } }) => {
-//   //   this.setState({ searchQuery: value });
-//   // };
-
-//   // handleSubmit = event => {
-//   //   event.preventDefault();
-//   //   console.log(this.state.searchQuery);
-
-//   render() {
-//     return (
-//       <div>
-//         <header>
-//           <form onSubmit={}>
-//             <input
-//               type="text"
-//               autoComplete="off"
-//               autoFocus
-//               placeholder="Search images and photos"
-//               // onChange={this.handleChange}
-//               // value={this.state.searchQuery}
-//             />
-
-//             <button type="submit">Search</button>
-//           </form>
-//         </header>
-//       </div>
-//     );
-//   }
-//   };
+// const Searchbar = ({ searchQuery }) => {
+//   const inputValue = searchQuery;
+//   return (
+//     <div>
+//       <form>
+//         <input type="text" />
+//         <button type="submit">Search</button>
+//       </form>
+//     </div>
+//   );
+// };
 
 // export default Searchbar;
-
-const Searchbar = ({ searchQuery }) => {
-  const inputValue = searchQuery;
-  return (
-    <div>
-      <form>
-        <input type="text" />
-        <button type="submit">Search</button>
-      </form>
-    </div>
-  );
-};
-
-export default Searchbar;
